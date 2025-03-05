f6960f9a87e40e68472d18c697bee1e99c64a119
argifatur <argifatur@gmail.com>
2025-03-05 20:36:50 +0800

    update: pdf

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Webklex\PDFMerger\Facades\PDFMergerFacade as PDFMerger;
 use Spatie\PdfToText\Pdf;
+use PHPHtmlParser\Dom;
+
 
 class PdfController extends Controller
 {
@@ -13,12 +15,12 @@ class PdfController extends Controller
     {
         return view('backend.create');
     }
-    
+
     public function merge()
     {
         return view('backend.merge');
     }
-    
+
     public function pdfToText()
     {
         // $text = (new Pdf("C:\Program Files\Git\mingw64\bin\pdftotext.exe"))
@@ -27,7 +29,7 @@ class PdfController extends Controller
         // return $text;
         return view('backend.pdf-to-text');
     }
-    
+
     public function prosesPdfToText(Request $request)
     {
         $this->validate($request, [
@@ -48,17 +50,17 @@ class PdfController extends Controller
         return $text;
         return back()->with('message', 'Berhasil Konversi');
     }
-    
+
     public function filepond()
     {
         return view('backend.filepond');
     }
-    
+
     public function uploadMerge()
     {
         return view('backend.uploadmerge');
     }
-    
+
     public function prosesMerge(Request $request)
     {
         $this->validate($request, [
@@ -78,13 +80,14 @@ class PdfController extends Controller
             $pdf->save(public_path($fileName));
         }
 
-        $filePath = url($fileName); // Generate URL for the merged PDF
+        $filePath = secure_url($fileName); // Generate URL for the merged PDF
+        // return $filePath;
 
         // Return a view with the preview link
         return view('pdf.preview', ['filePath' => $filePath]);
     }
 
-    
+
     public function prosesMergeFilepond(Request $request)
     {
         $request->validate([
@@ -117,25 +120,25 @@ class PdfController extends Controller
 
         return response()->download($outputPath)->deleteFileAfterSend(true);
     }
-    
+
     public function prosesUpload(Request $request)
     {
         if($request->hasFile('file')){
 
             $uploadPath = "uploads/gallery/";
-    
+
             $file = $request->file('file');
-    
+
             $extention = $file->getClientOriginalExtension();
             $filename = time().'-'.rand(0,99).'.'.$extention;
             $file->move($uploadPath, $filename);
-    
+
             $finalImageName = $uploadPath.$filename;
-    
+
             Gallery::create([
                 'image' => $finalImageName
             ]);
-    
+
             return response()->json(['success' => 'Image Uploaded Successfully']);
         }
         else
@@ -143,4 +146,93 @@ class PdfController extends Controller
             return response()->json(['error' => 'File upload failed.']);
         }
     }
+
+    public function htmlParser(Request $request)
+    {
+        $dom = new \DOMDocument();
+        // $htmlContent = file_get_contents('https://sps-inaportnet.dephub.go.id/index.php/builtin/manage/spog/cetak/SPS.SPOG.IDSRI.2409.002728');
+        $htmlContent = file_get_contents('https://sps-inaportnet.dephub.go.id/index.php/builtin/manage/spog/cetak/SPS.SPOG.IDSRI.2411.015677');
+        @$dom->loadHTML($htmlContent);
+
+        $xpath = new \DOMXPath($dom);
+
+        // Ekstrak data menggunakan XPath
+        $noSurat = $xpath->query("//div[contains(text(), 'No :')]/text()")->item(0)->textContent ?? '';
+        $noSurat = trim(str_replace('No : ', '', $noSurat)); // Menghapus 'No : ' dari hasil
+
+        $namaKapal = $xpath->query("//td[contains(text(), 'Nama Kapal')]/following-sibling::td")->item(0)->textContent ?? '';
+        $jenisKapal = $xpath->query("//td[contains(text(), 'Jenis Kapal')]/following-sibling::td")->item(0)->textContent ?? '';
+        $bendera = $xpath->query("//td[contains(text(), 'Bendera')]/following-sibling::td")->item(0)->textContent ?? '';
+        $isiKotor = $xpath->query("//td[contains(text(), 'Isi Kotor')]/following-sibling::td")->item(0)->textContent ?? '';
+        $nahkoda = $xpath->query("//td[contains(text(), 'Nakhoda')]/following-sibling::td")->item(0)->textContent ?? '';
+        $agen = $xpath->query("//td[contains(text(), 'Milik / Agen')]/following-sibling::td")->item(0)->textContent ?? '';
+        $bergerakDari = $xpath->query("//td[contains(text(), 'Untuk bergerak dari')]/following-sibling::td")->item(0)->textContent ?? '';
+        $waktuGerak = $xpath->query("//td[contains(text(), 'Waktu Gerak')]/following-sibling::td")->item(0)->textContent ?? '';
+        $keperluan = $xpath->query("//td[contains(text(), 'Keperluan')]/following-sibling::td")->item(0)->textContent ?? '';
+        $namaPandu = $xpath->query("//td[contains(text(), 'Nama Pandu')]/following-sibling::td")->item(0)->textContent ?? '';
+
+        return [
+            'no_surat' => $noSurat,
+            'nama_kapal' => str_replace(': ', '', trim($namaKapal)),
+            'jenis_kapal' => str_replace(': ', '', trim($jenisKapal)),
+            'bendera' => str_replace(': ', '', trim($bendera)),
+            'isi_kotor' => str_replace(': ', '', trim($isiKotor)),
+            'nahkoda' => str_replace(': ', '', trim($nahkoda)),
+            'agen' => str_replace(': ', '', trim($agen)),
+            'bergerak_dari' => str_replace(': ', '', trim($bergerakDari)),
+            'waktu_gerak' => str_replace(': ', '', trim($waktuGerak)),
+            'keperluan' => str_replace(': ', '', trim($keperluan)),
+            'nama_pandu' => str_replace(': ', '', trim($namaPandu)),
+        ];
+    }
+
+    public function pdfQr(){
+        return view('backend.pdf-qr');
+    }
+
+    public function prosesPdfQr(Request $request){
+        $validator = Validator::make($request->all(), [
+            'pdf_file' => 'required|mimes:pdf|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $path = $request->file('pdf_file')->store('uploads', 'public');
+
+        $parser = new Parser();
+        $pdf = $parser->parseFile(storage_path('app/public/' . $path));
+
+        $qrCodeReader = new QrCodeReader();
+
+        foreach ($pdf->getPages() as $page) {
+            $images = $page->getImages();
+
+            foreach ($images as $image) {
+                $imagePath = storage_path('app/public/temp/' . uniqid() . '.png');
+                file_put_contents($imagePath, $image->getPngData());
+
+                try {
+                    $result = $qrCodeReader->readFromFile($imagePath);
+                    if ($result) {
+                        unlink($imagePath);
+
+                        Storage::disk('public')->delete($path);
+
+                        return response()->json(['qr_code_content' => $result->getText()]);
+                    }
+                } catch (\Exception $e) {
+                    unlink($imagePath);
+                }
+            }
+        }
+
+        // Hapus file PDF yang diunggah jika QR code tidak ditemukan
+        Storage::disk('public')->delete($path);
+
+        // Kembalikan pesan error jika QR code tidak ditemukan
+        return response()->json(['error' => 'QR code tidak ditemukan dalam file PDF'], 404);
+    }
+
 }
